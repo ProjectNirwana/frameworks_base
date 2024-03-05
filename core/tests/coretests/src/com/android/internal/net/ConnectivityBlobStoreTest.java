bionic-linux/android_frameworks_base

Implement ConnectivityBlobStore list
Implement list(), including tests.

Bug: 307903113
Test: atest ConnectivityBlobStoreTest
Change-Id: Iac799d37fd69ed008b0d8972e8320308e4451a1c

core/tests/coretests/src/com/android/internal/net/ConnectivityBlobStoreTest.java
@@ -111,4 +111,27 @@ public class ConnectivityBlobStoreTest {
 
         assertFalse(mConnectivityBlobStore.remove(name1));
     }
+
+    @Test
+    public void testList() throws Exception {
+        final String[] unsortedNames = new String[] {
+                TEST_NAME + "1",
+                TEST_NAME + "2",
+                TEST_NAME + "0",
+                "NON_MATCHING_PREFIX",
+                "MATCHING_SUFFIX_" + TEST_NAME
+        };
+        // Expected to match and discard the prefix and be in increasing sorted order.
+        final String[] expected = new String[] {
+                "0",
+                "1",
+                "2"
+        };
+
+        for (int i = 0; i < unsortedNames.length; i++) {
+            assertTrue(mConnectivityBlobStore.put(unsortedNames[i], TEST_BLOB));
+        }
+        final String[] actual = mConnectivityBlobStore.list(TEST_NAME /* prefix */);
+        assertArrayEquals(expected, actual);
+    }
 }

ProjectNirwana/frameworks_base

core/tests/coretests/src/com/android/internal/net/ConnectivityBlobStoreTest.java
@@ -129,4 +129,28 @@ public class ConnectivityBlobStoreTest {
         assertNull(connectivityBlobStore.get(name2));
         assertFalse(connectivityBlobStore.remove(name2));
     }
+
+    @Test
+    public void testList() throws Exception {
+        final String[] unsortedNames = new String[] {
+                TEST_NAME + "1",
+                TEST_NAME + "2",
+                TEST_NAME + "0",
+                "NON_MATCHING_PREFIX",
+                "MATCHING_SUFFIX_" + TEST_NAME
+        };
+        // Expected to match and discard the prefix and be in increasing sorted order.
+        final String[] expected = new String[] {
+                "0",
+                "1",
+                "2"
+        };
+        final ConnectivityBlobStore connectivityBlobStore = createConnectivityBlobStore();
+
+        for (int i = 0; i < unsortedNames.length; i++) {
+            assertTrue(connectivityBlobStore.put(unsortedNames[i], TEST_BLOB));
+        }
+        final String[] actual = connectivityBlobStore.list(TEST_NAME /* prefix */);
+        assertArrayEquals(expected, actual);
+    }
 }